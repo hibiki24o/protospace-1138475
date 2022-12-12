872acabfbf653277dc6a12988a5c502c3a11b596
hibiki24o <lukasu0624@gmail.com>
2022-12-12 18:18:52 +0900

    修正３回目

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,7 @@
 class CommentsController < ApplicationController
 
   def create
-    Comment.create(comment_params)
-    @comment = Comment.new(comment_params)
+    @comment = Comment.create(comment_params)
     if @comment.save
       redirect_to  "/prototypes/#{@comment.prototype_id}"
     else
